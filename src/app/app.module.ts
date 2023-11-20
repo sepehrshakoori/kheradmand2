a64bd318bf2bd8179f10a576e17b20d337784caa
Sepehr Shakoori <S.shakoori@persianswitch.net>
2023-11-20 15:26:52 +0330

    expense-manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseEntryComponent,
+
+
   ],
   imports: [
     BrowserModule
